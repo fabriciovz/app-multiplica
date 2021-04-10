39f7c94343afb24ed1476f881af367bc722d638f
Fabricio Bravo <vn0de9e@M-C02D1G8DMD6M.local>
2021-04-10 08:09:58 -0400

    try to deploy in heroku -refactor

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/app-multiplica'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/app-multiplica'));
 
-app.get('/*', (req,res,next) => {
-    res.sendFile(path.join(__dirname + '/dist/app-multiplica/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/app-multiplica/'}),
+);
 
-
-app.listen(process.env.PORT || 8000);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
