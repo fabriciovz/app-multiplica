6564c3def3976cb54eb6797714dd8f9f24487562
Fabricio Bravo <vn0de9e@M-C02D1G8DMD6M.local>
2021-04-12 21:55:25 -0400

    testing with api-multiplica backend deployed on heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  url_api: 'http://localhost:8024/api'
-  //url_api: 'https://api-multiplica.herokuapp.com'
+  //url_api: 'http://localhost:8024/api'
+  url_api: 'https://api-multiplica.herokuapp.com/api'
+
 };
 
 /*
